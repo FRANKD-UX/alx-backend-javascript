84f7f6762360596a0d37e844137ddb209495fe33
FRANKD-UX <frankdafrica@gmail.com>
2025-06-14 01:26:51 +0000

    push

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,7 +3,7 @@
  * @param {String} msg The message to display.
  * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
-const displayMessage = (msg) => {
+const displayMessage = () => {
   console.log(msg);
 };
 
